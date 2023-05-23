c06314294a8728601d042c69451115ed31a4f0e8
Raphael Simonetti <raphasimonettti@gmail,com>
2023-05-23 16:50:56 -0300

    Aplicativo Bancario

---
+++ Aplicativobancario/Aplicativo Bancario/Aplicativo Bancario/Operacoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicativo_Bancario
+{
+    internal class Operacoes
+    {
+         
+
+        public string mostrarMenu()
+        {
+            string selecionada;
+            Console.WriteLine("Menu de operação");
+            Console.WriteLine("[a] Mostrar saldo");
+            Console.WriteLine("[b] Efetuar Deposito");
+            Console.WriteLine("[c] Efetuar Saque");
+            Console.WriteLine("[d]Finalizar");
+            selecionada = Console.ReadLine();
+            return selecionada;
+
+        }
+    }
+}
---
+++ Aplicativobancario/Aplicativo Bancario/Aplicativo Bancario/Program.cs
@@ -0,0 +1,67 @@
+﻿using Aplicativo_Bancario;
+
+internal class Program
+{
+ 
+    private static void Main()
+    {
+        string opcao_selecionda;
+        double valor;
+        double saldo;
+        bool encerrar;
+        Console.WriteLine("Digite o seu saldo");
+        saldo = Convert.ToDouble(Console.ReadLine());
+
+        encerrar = false;
+        
+        Operacoes metodo = new Operacoes();
+       
+
+        while(encerrar == false)
+        {
+
+           opcao_selecionda = metodo.mostrarMenu();   
+            if(opcao_selecionda == "a")
+            {
+                Console.WriteLine(saldo);
+            }
+            else if(opcao_selecionda=="b")
+            {
+                Console.WriteLine("Digite o valor a depositar");
+                valor = Convert.ToDouble(Console.ReadLine());
+                saldo = valor + saldo;
+                Console.WriteLine("valor depositado");
+            }
+            else if (opcao_selecionda == "c")
+            {
+                Console.WriteLine("Digite o valor a ser sacado");
+                valor = Convert.ToDouble(Console.ReadLine());
+                if(valor > saldo)
+                {
+                    Console.WriteLine("Saldo insuficiente)");
+
+                }
+                else
+                {
+                    saldo = saldo - valor;
+                    Console.WriteLine("Saque realizado");
+                }
+            }
+            else if(opcao_selecionda == "d")
+            {
+                encerrar = true;
+            }
+            else
+            {
+                Console.Write("Digite a,b,c ou d");
+            }
+            
+
+        }
+
+
+    }
+   
+
+   
+}
